25c75fadd455f71c1e1bdd17739cdf0256439234
CorSS_REST <39077480+atilanaufal@users.noreply.github.com>
2024-12-04 15:09:49 +0700

    Add files via upload

---
+++ Pertemuan 13/storage/framework/views/3a5f431b5b552dfc84865f94abcf7d8e.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    <h1>INI CONTACT</h1>
+    <h1>HUBUNGI SAYA IPSUM DOLOR</h1>
+    <nav>
+        <a href="<?php echo e(route('home')); ?>">Home</a>
+        <a href="<?php echo e(route('about')); ?>">About</a>
+        <a href="<?php echo e(route('contact')); ?>">Contact</a>
+    </nav>
+</body>
+</html>
+<?php /**PATH J:\VScode Project\Komputasi Wb\pemrograman-web-a\resources\views/contact.blade.php ENDPATH**/ ?>
---
+++ Pertemuan 13/storage/framework/views/542585b801fb05f4b4b1574632132af3.php
@@ -0,0 +1,14 @@
+<?php $__env->startSection('title', 'Home - Aplikasi Laravel'); ?>
+
+<?php $__env->startSection('content'); ?>
+    <p class="mt-5">
+        <strong>Ini adalah halaman home</strong>
+        <br>
+        Halaman ini terdiri atas ...
+    </p>
+
+
+<?php $__env->stopSection(); ?>
+
+
+<?php echo $__env->make('layoutp13.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH J:\VScode Project\Komputasi Wb\pemrograman-web-a\resources\views/home_p13.blade.php ENDPATH**/ ?>
